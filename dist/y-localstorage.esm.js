86deb73082b229b8c0027e84cf9005a5f198aa91
Andreas Rozek <A.Rozek@gmx.de>
2023-06-29 17:01:17 +0200

    new build

--- dist/y-localstorage.esm.js
+++ dist/y-localstorage.esm.js
@@ -3,6 +3,7 @@ import { Observable } from 'lib0/observable';
 
 var LocalStorageProvider;
 (function (LocalStorageProvider_1) {
+    const GUIDPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
     class LocalStorageProvider extends Observable {
         constructor(DocName, sharedDoc, CounterLimit = 5) {
             super();
@@ -10,6 +11,7 @@ var LocalStorageProvider;
             this._CounterLimit = 5;
             this._pendingUpdates = 0;
             this._completedUpdates = 0;
+            this._SubDocMap = new WeakMap();
             this._DocPrefix = DocName + '-';
             this._sharedDoc = sharedDoc;
             this._UpdateCounter = 0; // will be updated by "_applyStoredUpdates"
@@ -22,6 +24,8 @@ var LocalStorageProvider;
             }
             this._storeUpdate = this._storeUpdate.bind(this);
             sharedDoc.on('update', this._storeUpdate);
+            this._manageSubDocs = this._manageSubDocs.bind(this);
+            sharedDoc.on('subdocs', this._manageSubDocs);
             this.destroy = this.destroy.bind(this);
             sharedDoc.on('destroy', this.destroy);
         }
@@ -35,7 +39,9 @@ var LocalStorageProvider;
                 return;
             } // persistence no longer exists
             this._removeStoredUpdatesStartingWith(0);
+            this._removeStoredSubDocs();
             this._sharedDoc.off('update', this._storeUpdate);
+            this._sharedDoc.off('subdocs', this._manageSubDocs);
             this._sharedDoc.off('destroy', this.destroy);
             if (!this.isSynced) {
                 this._pendingUpdates = 0;
@@ -81,7 +87,7 @@ var LocalStorageProvider;
                         this._DocPrefix + this._UpdateCounter, JSON.stringify(Array.from(Update)));
                         this._UpdateCounter++;
                     }
-                    else { // compact previous and current update
+                    else { // compact previous and current updates
                         localStorage.setItem(// may fail
                         this._DocPrefix + 0, JSON.stringify(Array.from(Y.encodeStateAsUpdate(this._sharedDoc))));
                         this._removeStoredUpdatesStartingWith(1);
@@ -97,18 +103,37 @@ var LocalStorageProvider;
         /**** _removeStoredUpdatesStartingWith - removes stored (incremental) updates ****/
         _removeStoredUpdatesStartingWith(minimalIndex) {
             const PrefixLength = this._DocPrefix.length;
-            try {
-                this._StorageKeys().forEach((Key) => {
-                    let UpdateIndex = parseInt(Key.slice(PrefixLength), 10);
-                    if (UpdateIndex >= minimalIndex) {
+            let lastFailure = undefined;
+            this._StorageKeys().forEach((Key) => {
+                let UpdateIndex = parseInt(Key.slice(PrefixLength), 10);
+                if (UpdateIndex >= minimalIndex) {
+                    try {
                         localStorage.removeItem(Key); // may fail
                     }
-                });
-            }
-            catch (Signal) {
-                console.warn('y-localstorage: could not clean-up localstorage, reason: ' + Signal);
+                    catch (Signal) {
+                        lastFailure = Signal;
+                    }
+                }
+            });
+            if (lastFailure != null) {
+                console.warn('y-localstorage: could not clean-up localstorage, reason: ' + lastFailure);
             }
             this._UpdateCounter = minimalIndex;
+        }
+        /**** _removeStoredSubDocs - removes any stored subdocs ****/
+        _removeStoredSubDocs() {
+            let lastFailure = undefined;
+            this._StorageSubKeys().forEach((Key) => {
+                try {
+                    localStorage.removeItem(Key); // may fail
+                }
+                catch (Signal) {
+                    lastFailure = Signal;
+                }
+            });
+            if (lastFailure != null) {
+                console.warn('y-localstorage: could not clean-up localstorage, reason: ' + lastFailure);
+            }
         }
         /**** _breakdownWith - breaks down this provider after failure ****/
         _breakdownWith(Message, Reason) {
@@ -119,6 +144,25 @@ var LocalStorageProvider;
                 this.emit('sync-aborted', [this, 1.0]);
             }
             throw new Error(Message + (Reason == null ? '' : ', reason: ' + Reason));
+        }
+        /**** _manageSubDocs - manages subdoc persistences ****/
+        _manageSubDocs(Changes) {
+            const providePersistenceFor = (SubDoc) => {
+                if (!this._SubDocMap.has(SubDoc)) {
+                    const SubDocProvider = new LocalStorageProvider(this._DocPrefix.slice(0, -1) + '.' + SubDoc.guid, SubDoc, this._CounterLimit);
+                    this._SubDocMap.set(SubDoc, SubDocProvider);
+                }
+            };
+            const { added, removed, loaded } = Changes;
+            if (added != null) {
+                added.forEach((SubDoc) => providePersistenceFor(SubDoc));
+            }
+            if (removed != null) {
+                removed.forEach((SubDoc) => this._SubDocMap.delete(SubDoc));
+            }
+            if (loaded != null) {
+                loaded.forEach((SubDoc) => providePersistenceFor(SubDoc));
+            }
         }
         /**** _reportProgress - emits events reporting synchronization progress ****/
         _reportProgress() {
@@ -140,7 +184,7 @@ var LocalStorageProvider;
                     this.emit('sync-continued', [this, Progress]);
             }
         }
-        /**** _StorageKeys - lists all keys used by this provider ****/
+        /**** _StorageKeys - lists all keys used for sharedDoc itself ****/
         _StorageKeys() {
             const PrefixLength = this._DocPrefix.length;
             const Result = [];
@@ -148,6 +192,20 @@ var LocalStorageProvider;
                 const Key = localStorage.key(i);
                 if (Key.startsWith(this._DocPrefix) &&
                     (/^\d+$/.test(Key.slice(PrefixLength)) === true)) {
+                    Result.push(Key);
+                }
+            }
+            return Result;
+        }
+        /**** _StorageSubKeys - lists all keys used for subdocs of sharedDoc ****/
+        _StorageSubKeys() {
+            const DocPrefix = this._DocPrefix.slice(0, -1) + '.';
+            const PrefixLength = DocPrefix.length;
+            const Result = [];
+            for (let i = 0, l = localStorage.length; i < l; i++) {
+                const Key = localStorage.key(i);
+                if (Key.startsWith(DocPrefix) &&
+                    (GUIDPattern.test(Key.slice(PrefixLength)) === true)) {
                     Result.push(Key);
                 }
             }
